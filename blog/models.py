fb11d0361414cb36f3dfc0b3bcb18a4a77262fd0
lubakach <lubakach26@gmail.com>
2020-05-05 17:43:24 +0300

    Check

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,27 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    quote = models.CharField(max_length=1000)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name = models.CharField('имя автора', max_length=50)
+    comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
